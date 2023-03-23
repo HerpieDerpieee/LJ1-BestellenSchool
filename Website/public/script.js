3dec869bab9925f330fb53e87886fc377de30700
Valentijn :) <120188387+HerpieDerpieee@users.noreply.github.com>
2023-03-23 10:07:01 +0100

    homepage responsive design

--- Website/public/script.js
+++ Website/public/script.js
@@ -1,9 +1,22 @@
 var slides = document.querySelectorAll('.slide');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide, 3000);
+let slidesRoot = document.getElementById("slider");
+var r = document.querySelector(':root');
+let img = document.getElementById("imgSlider");
 
 function nextSlide() {
     slides[currentSlide].className = 'slide';
     currentSlide = (currentSlide + 1) % slides.length;
     slides[currentSlide].className = 'slide active';
+}
+
+addEventListener("resize", () => {
+    console.log(img.offsetHeight);
+    r.style.setProperty('--slider-height', img.offsetHeight + "px");
+})
+
+function kaasblokje(){
+    console.log(img.offsetHeight);
+    r.style.setProperty('--slider-height', img.offsetHeight + "px");
 }
